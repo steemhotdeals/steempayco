c31092209aef9846b3a5308e8c62bb53fbe6b910
ayogom <40152930+ayogom@users.noreply.github.com>
2020-12-07 13:13:58 +0900

    Update pay.jsx

--- legacy/jsx/pay.jsx
+++ legacy/jsx/pay.jsx
@@ -129,7 +129,7 @@ var upbeatPriceFeed = (callback) => {
     .then(res => res.json())
     .then(
         (result) => {
-            var average = result.steem.eur);
+            var average = result[0].steem.eur);
             callback(average);
         },
         (error) => {
@@ -188,7 +188,7 @@ class Receiver extends React.Component {
         .then(res => res.json())
         .then(
             (result) => {
-                var average = result.steem.eur);
+                var average = result[0].steem.eur);
                 var date = new Date();
                 this.setState({price: Math.round(average), lastFeedUpdate: date.toLocaleDateString() + " " + date.toLocaleTimeString()});
             },
