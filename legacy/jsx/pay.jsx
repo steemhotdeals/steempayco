12cf719f41fe8eccca1636a8e7b0779c447c95d4
ayogom <40152930+ayogom@users.noreply.github.com>
2020-12-08 10:35:53 +0900

    Update pay.jsx

--- legacy/jsx/pay.jsx
+++ legacy/jsx/pay.jsx
@@ -192,7 +192,8 @@ class Receiver extends React.Component {
                 var date = new Date();		
 	        var average = result.ticker.price;
 	        console.log("2번항목 : "+average);
-                this.setState({price: Math.round(average), lastFeedUpdate: date.toLocaleDateString() + " " + date.toLocaleTimeString()});
+		console.log("3번항목 " "+result.ticker.price);
+                this.setState({price: Math.round(result.ticker.price), lastFeedUpdate: date.toLocaleDateString() + " " + date.toLocaleTimeString()});
             },
             (error) => {
                 alert("Critical Error! Please retry later." + error)
